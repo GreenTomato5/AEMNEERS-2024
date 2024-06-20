6dff79ceb6212e35c137dece55f095854e950023
Otto <grentomato@gmail.com>
2024-06-20 10:36:10 -0800

    Fix L code

--- src/main/java/frc/robot/subsystems/pivot/Pivot.java
+++ src/main/java/frc/robot/subsystems/pivot/Pivot.java
@@ -16,97 +16,98 @@ import org.littletonrobotics.junction.Logger;
 // Subsystem base automatically runs periodic, too scared to put stuff in robot.java
 public class Pivot extends SubsystemBase {
 
-    private final PivotIOInputsAutoLogged inputs = new PivotIOInputsAutoLogged();
-    private Mechanism2d intakePivot;
-    private MechanismRoot2d root;
-    private MechanismLigament2d rotatingLigament;
+  private final PivotIOInputsAutoLogged inputs = new PivotIOInputsAutoLogged();
+  private Mechanism2d intakePivot;
+  private MechanismRoot2d root;
+  private MechanismLigament2d rotatingLigament;
 
-    SysIdRoutine sysId;
-    PivotIO io;
+  SysIdRoutine sysId;
+  PivotIO io;
 
-    public Pivot(PivotIO io) {
-        this.io = io;
-        // TODO: Tune feedback controllers, these values are MADE UP
-        switch (Constants.currentMode) {
-            case REAL:
-                io.configurePID(1.0, 0, 0);
-                break;
-            case REPLAY:
-                io.configurePID(1.0, 0.0, 0.0);
-                break;
-            case SIM:
-                io.configurePID(3.0, 0.0, 0.1);
-                break;
-            default:
-                break;
-        }
-
-        setUp2dMechSim();
-        // Using SysID is wild
-        sysId = new SysIdRoutine(
-                new SysIdRoutine.Config(
-                        null,
-                        null,
-                        null,
-                        (state) -> Logger.recordOutput("Pivot/SysIdState", state.toString())),
-                new SysIdRoutine.Mechanism((voltage) -> runVolts(voltage.in(Volts)), null, this));
+  public Pivot(PivotIO io) {
+    this.io = io;
+    // TODO: Tune feedback controllers, these values are MADE UP
+    switch (Constants.currentMode) {
+      case REAL:
+        io.configurePID(1.0, 0, 0);
+        break;
+      case REPLAY:
+        io.configurePID(1.0, 0.0, 0.0);
+        break;
+      case SIM:
+        io.configurePID(3.0, 0.0, 0.1);
+        break;
+      default:
+        break;
     }
 
-    private void runVolts(double volts) {
-        io.setVoltage(volts);
-    }
+    setUp2dMechSim();
+    // Using SysID is wild
+    sysId =
+        new SysIdRoutine(
+            new SysIdRoutine.Config(
+                null,
+                null,
+                null,
+                (state) -> Logger.recordOutput("Pivot/SysIdState", state.toString())),
+            new SysIdRoutine.Mechanism((voltage) -> runVolts(voltage.in(Volts)), null, this));
+  }
 
-    // IN RADIANS!!!!!!!!!!!!!!!!!!!!!!!!!!
-    public void setPosition(Double position) {
-        io.setPosition(position);
-    }
+  private void runVolts(double volts) {
+    io.setVoltage(volts);
+  }
 
-    public Command setPositionCommand(DoubleSupplier posRad) {
-        return run(() -> setPosition(posRad.getAsDouble()));
-    }
+  // IN RADIANS!!!!!!!!!!!!!!!!!!!!!!!!!!
+  public void setPosition(Double position) {
+    io.setPosition(position);
+  }
 
-    public Command getDefaultCommand() {
-        return setPositionCommand(() -> 0.0);
-    }
+  public Command setPositionCommand(DoubleSupplier posRad) {
+    return run(() -> setPosition(posRad.getAsDouble()));
+  }
 
-    public boolean nearSetPoint() {
-        return io.nearSetPoint();
-    }
+  public Command getDefaultCommand() {
+    return setPositionCommand(() -> 0.0);
+  }
 
-    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
-        return sysId.quasistatic(direction);
-    }
+  public boolean nearSetPoint() {
+    return io.nearSetPoint();
+  }
 
-    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
-        return sysId.dynamic(direction);
-    }
+  public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+    return sysId.quasistatic(direction);
+  }
 
-    // TODO: Ok so do this but for a 3d mech sim, see advantage scope docs to
-    // understand
-    public void setUp2dMechSim() {
-        intakePivot = new Mechanism2d(10, 10);
-        root = intakePivot.getRoot("Root", 5, 5);
+  public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+    return sysId.dynamic(direction);
+  }
 
-        rotatingLigament = new MechanismLigament2d("RotatingLigament", 7, 0);
-        root.append(rotatingLigament);
-    }
+  // TODO: Ok so do this but for a 3d mech sim, see advantage scope docs to
+  // understand
+  public void setUp2dMechSim() {
+    intakePivot = new Mechanism2d(10, 10);
+    root = intakePivot.getRoot("Root", 5, 5);
 
-    public void update2dMechSim() {
-        // negative so it looks like the intake instead of the way the motor is turning
-        // if that makes
-        // senseP
-        rotatingLigament.setAngle(
-                -Math.toDegrees(io.getPivotPosition()) - Math.toDegrees(Constants.Pivot.SIMOFFSET));
-        Logger.recordOutput("Intake Pivot Mechanism", intakePivot);
-    }
+    rotatingLigament = new MechanismLigament2d("RotatingLigament", 7, 0);
+    root.append(rotatingLigament);
+  }
 
-    @Override
-    public void periodic() {
-        update2dMechSim();
-        io.updateInputs(inputs);
-        Logger.processInputs("Pivot", inputs);
-        Logger.recordOutput(
-                "Pivot/Running Command",
-                Optional.ofNullable(this.getCurrentCommand()).map(Command::getName).orElse("None"));
-    }
+  public void update2dMechSim() {
+    // negative so it looks like the intake instead of the way the motor is turning
+    // if that makes
+    // senseP
+    rotatingLigament.setAngle(
+        -Math.toDegrees(io.getPivotPosition()) - Math.toDegrees(Constants.Pivot.SIMOFFSET));
+    Logger.recordOutput("Intake Pivot Mechanism", intakePivot);
+  }
+
+  @Override
+  public void periodic() {
+    update2dMechSim();
+    io.updateInputs(inputs);
+    Logger.processInputs("Pivot", inputs);
+    Logger.recordOutput(
+        "Pivot/Running Command",
+        Optional.ofNullable(this.getCurrentCommand()).map(Command::getName).orElse("None"));
+  }
 }
