10eadcb92b2da137ffe07d71a8e6800227994157
Liquid2112 <Shubhpatel2504@gmail.com>
2024-06-22 22:18:44 -0500

    Done with amp bar and amp shooter

--- src/main/java/frc/robot/subsystems/amp/Amp.java
+++ src/main/java/frc/robot/subsystems/amp/Amp.java
@@ -2,22 +2,156 @@ package frc.robot.subsystems.amp;
 
 import static edu.wpi.first.units.Units.*;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Constants;
-import frc.robot.subsystems.pivot.PivotIOInputsAutoLogged;
-import frc.robot.subsystems.shooter.Shooter;
-import frc.robot.subsystems.shooter.ShooterIOInputsAutoLogged;
-
 import java.util.Optional;
 import java.util.function.DoubleSupplier;
 import org.littletonrobotics.junction.Logger;
 
+// Subsystem base automatically runs periodic, too scared to put stuff in robot.java
 public class Amp extends SubsystemBase {
-  private Amp io;
-  private SysIdRoutine sysId;
-  private final ShooterIOInputsAutoLogged shooterInputs = new ShooterIOInputsAutoLogged();
-  private final PivotIOInputsAutoLogged pivotInputs = new PivotIOInputsAutoLogged();
 
+  private final AmpIOInputsAutoLogged inputs = new AmpIOInputsAutoLogged();
+  private Mechanism2d ampBarPivot;
+  private MechanismRoot2d root;
+  private MechanismLigament2d rotatingLigament;
+
+  private Pose3d sim3dPose;
+  private Translation3d zeroedTranslation3d;
+  private Pose3d zeroedPose3d;
+
+  SysIdRoutine sysId;
+  AmpIO io;
+
+  public Amp(AmpIO io) {
+    this.io = io;
+    // TODO: Tune feedback controllers, these values are MADE UP
+    switch (Constants.currentMode) {
+      case REAL:
+        io.configurePID(1.0, 0, 0);
+        break;
+      case REPLAY:
+        io.configurePID(1.0, 0.0, 0.0);
+        break;
+      case SIM:
+        io.configurePID(3.0, 0.0, 0.1);
+        break;
+      default:
+        break;
+    }
+
+    setUp2dMechSim();
+    // Using SysID is wild
+    sysId =
+        new SysIdRoutine(
+            new SysIdRoutine.Config(
+                null,
+                null,
+                null,
+                (state) -> Logger.recordOutput("Pivot/SysIdState", state.toString())),
+            new SysIdRoutine.Mechanism((voltage) -> runVolts(voltage.in(Volts)), null, this));
+  }
+
+  private void runVolts(double volts) {
+    io.setVoltage(volts);
+  }
+
+  // IN RADIANS!!!!!!!!!!!!!!!!!!!!!!!!!!
+  public void setPosition(Double position) {
+    io.setPosition(position);
+  }
+
+  public void logPose3d(Translation3d robotTranslation3d) {
+    /**
+     * Zeroed with robot: x = 0.083 y = 0.22 z = 0.54
+     *
+     * <p>Zeroed With Axis: X = -0.23 y = 0.22 z = 0.34
+     */
+
+    // I dont think this is the right way to do it but i did it this way :)
+    zeroedTranslation3d = new Translation3d(0.313, 0, 0.2);
+
+    sim3dPose = new Pose3d(robotTranslation3d, new Rotation3d(0, io.getAmpBarPosition(), 0));
+    zeroedPose3d = new Pose3d(zeroedTranslation3d, sim3dPose.getRotation());
+
+    Logger.recordOutput("Pivot/Pivot 3d Pose", sim3dPose);
+    Logger.recordOutput("Pivot/Zeroed Pose", zeroedPose3d);
+  }
+  
+  public Command setPositionCommand(DoubleSupplier posRad) {
+    return run(() -> setPosition(posRad.getAsDouble()));
+  }
+
+  public Command getDefaultCommand() {
+    return setPositionCommand(() -> 0.0);
+  }
+
+  public boolean nearSetPoint() {
+    return io.nearSetPoint();
+  }
+
+  private void runSpinnerVolts(double volts) {
+    io.setSpinnerVoltage(volts);
+  }
+
+  public void setSpeed(Double speed) {
+    io.setSpeed(speed);
+  }
+
+  public Command setSpeedCommand(DoubleSupplier rps) {
+    return run(() -> setSpeed(rps.getAsDouble()));
+  }
+
+  public Command getSpinnerDefaultCommand() {
+    return setSpeedCommand(() -> 0.0);
+  }
+
+  public boolean nearSpeedPoint() {
+    return io.nearSpeedPoint();
+  }
+
+  public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+    return sysId.quasistatic(direction);
+  }
+
+  public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+    return sysId.dynamic(direction);
+  }
+
+  // TODO: Ok so do this but for a 3d mech sim, see advantage scope docs to
+  // understand
+  public void setUp2dMechSim() {
+    ampBarPivot = new Mechanism2d(10, 10);
+    root = ampBarPivot.getRoot("Root", 5, 5);
+
+    rotatingLigament = new MechanismLigament2d("RotatingLigament", 7, 0);
+    root.append(rotatingLigament);
+  }
+
+  public void update2dMechSim() {
+    // negative so it looks like the intake instead of the way the motor is turning
+    // if that makes
+    // senseP
+    rotatingLigament.setAngle(
+        -Math.toDegrees(io.getAmpBarPosition()) - Math.toDegrees(Constants.Pivot.SIMOFFSET));
+    Logger.recordOutput("Intake Pivot Mechanism", ampBarPivot);
+  }
+
+  @Override
+  public void periodic() {
+    update2dMechSim();
+    io.updateInputs(inputs);
+    Logger.processInputs("AmpBarPivot", inputs);
+    Logger.recordOutput(
+        "Pivot/Running Command",
+        Optional.ofNullable(this.getCurrentCommand()).map(Command::getName).orElse("None"));
+  }
 }
--- src/main/java/frc/robot/subsystems/amp/AmpIO.java
+++ src/main/java/frc/robot/subsystems/amp/AmpIO.java
@@ -1,5 +1,64 @@
 package frc.robot.subsystems.amp;
 
-public class AmpIO {
-    
+import org.littletonrobotics.junction.AutoLog;
+
+import frc.robot.subsystems.shooter.ShooterIO.ShooterIOInputs;
+
+public interface AmpIO {
+
+  @AutoLog
+  public static class AmpIOInputs {
+    public double ampBarCurrentPosition = 0.0;
+    public double ampBarAppliedVolts = 0.0;
+    public double ampBarSetpoint = 0.0;
+    public double ampBarVelocity = 0.0;
+    public double ampShooterVelocity = 0.0;
+    public double ampShooterSpeedPoint = 0.0;
+    public double ampShooterVoltage = 0.0;
+
+
+
+  }/** Updates the set of loggable inputs. */
+  public default void updateInputs(AmpIOInputs inputs) {}
+
+  /** Sets the angle of the amp bar, in radians. */
+  public default void setPosition(double positionRad) {}
+
+  /** Run open loop at the specified voltage. */
+  public default void setVoltage(double volts) {}
+
+  /** Stop in open loop. */
+  public default void stop() {}
+
+  /** Set velocity PID constants. */
+  public default void configurePID(double kP, double kI, double kD) {}
+
+  public default boolean nearSetPoint() {
+    return false;
+  }
+
+  public default double getAmpBarPosition() {
+    return 0.0;
+  }
+
+    public default void updateInputs(ShooterIOInputs inputs) {}
+
+  /** Sets the angle of the intake, in radians. */
+  public default void setSpeed(double rps) {}
+
+  /** Run open loop at the specified voltage. */
+  public default void setSpinnerVoltage(double volts) {}
+
+  /** Stop in open loop. */
+  public default void stopSpinner() {}
+
+  /** Set velocity PID constants. */
+  public default void configureSpinnerPID(double kP, double kI, double kD) {}
+
+  /** Set ff constants */
+  public default void configureFeedForward(double kS, double kV, double kA) {}
+
+  public default boolean nearSpeedPoint() {
+    return false;
+  }
 }
