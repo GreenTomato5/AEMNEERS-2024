3c8eb91dc41bb394fcf77b0a3de2d8177164392c
Otto <grentomato@gmail.com>
2024-06-22 21:02:48 -0800

    It climbs, im not gona test it but ok

--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -1,3 +1,40 @@
+package frc.robot.subsystems.climber;
+import org.littletonrobotics.junction.AutoLog;
+
 public interface ClimberIO {
-    
+    @AutoLog
+    public static class ClimberIOInputs {
+        public double leftClimberSpeed = 0.0;
+        public double rightClimberSpeed = 0.0;
+        public double leftClimberSetpointRads = 0.0;
+        public double rightClimberSetpointRads = 0.0;
+        public double leftClimberAppliedVolts = 0.0;
+        public double rightClimberAppliedVolts = 0.0;
+        public double leftClimberMeasuredRads = 0.0;
+        public double rightClimberMeasuredRads = 0.0;
+    }
+
+    public default void updateInputs(ClimberIOInputs inputs) {
+    }
+
+    public default void setRotations(double leftSetpoint, double rightSetpoint) {
+    }
+
+    public default void setVoltage(double volts) {
+    }
+
+    public default void stop() {
+    }
+
+    public default void configurePID(double kP, double kI, double kD) {
+    }
+
+    public default boolean leftClimberOverCurrentLimit(double currentLimit) {
+        return false;
+    }
+
+    public default boolean rightClimberOverCurrentLimit(double currentLimit) {
+        return false;
+    }
+
 }
