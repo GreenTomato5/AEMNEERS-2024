45017b954c0c18286371a20a41701aff8971049d
Otto <grentomato@gmail.com>
2024-06-23 08:01:40 -0800

    Fix AmpIO so its not bad

--- src/main/java/frc/robot/subsystems/amp/AmpIO.java
+++ src/main/java/frc/robot/subsystems/amp/AmpIO.java
@@ -2,63 +2,37 @@ package frc.robot.subsystems.amp;
 
 import org.littletonrobotics.junction.AutoLog;
 
-import frc.robot.subsystems.shooter.ShooterIO.ShooterIOInputs;
-
 public interface AmpIO {
 
   @AutoLog
   public static class AmpIOInputs {
-    public double ampBarCurrentPosition = 0.0;
-    public double ampBarAppliedVolts = 0.0;
-    public double ampBarSetpoint = 0.0;
-    public double ampBarVelocity = 0.0;
-    public double ampShooterVelocity = 0.0;
-    public double ampShooterSpeedPoint = 0.0;
-    public double ampShooterVoltage = 0.0;
-
-
-
-  }/** Updates the set of loggable inputs. */
+    public double pivotCurrentPosition = 0.0;
+    public double pivotAppliedVolts = 0.0;
+    public double pivotSetpoint = 0.0;
+    public double spinnerVelocity = 0.0;
+    public double spinnerSpeedPoint = 0.0;
+    public double spinnerAppliedVolts = 0.0;
+  }
+  
+  /** Updates the set of loggable inputs. */
   public default void updateInputs(AmpIOInputs inputs) {}
 
-  /** Sets the angle of the amp bar, in radians. */
-  public default void setPosition(double positionRad) {}
 
-  /** Run open loop at the specified voltage. */
-  public default void setVoltage(double volts) {}
+  // Pivot Stuff
+  public default void setPivotPosition(double positionRad) {}
 
-  /** Stop in open loop. */
-  public default void stop() {}
+  public default void setPivotVoltage(double volts) {}
 
-  /** Set velocity PID constants. */
-  public default void configurePID(double kP, double kI, double kD) {}
+  public default void stopPivot() {}
 
-  public default boolean nearSetPoint() {
-    return false;
-  }
+  public default void configurePivotPID(double kP, double kI, double kD) {}
 
-  public default double getAmpBarPosition() {
-    return 0.0;
-  }
+  // Bar Spinner Stuff
+  public default void setSpinnerSpeed(double rps) {}
 
-    public default void updateInputs(ShooterIOInputs inputs) {}
-
-  /** Sets the angle of the intake, in radians. */
-  public default void setSpeed(double rps) {}
-
-  /** Run open loop at the specified voltage. */
   public default void setSpinnerVoltage(double volts) {}
 
-  /** Stop in open loop. */
   public default void stopSpinner() {}
 
-  /** Set velocity PID constants. */
   public default void configureSpinnerPID(double kP, double kI, double kD) {}
-
-  /** Set ff constants */
-  public default void configureFeedForward(double kS, double kV, double kA) {}
-
-  public default boolean nearSpeedPoint() {
-    return false;
-  }
 }
